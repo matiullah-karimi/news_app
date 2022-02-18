26328e9468ab1a3b22bd5a4082a72a6b68ac8dc2
Matiullah Karimi <matiullah.karimi@gmail.com>
2022-02-18 23:55:22 +0400

    Add news list item widget

--- lib/widgets/news_list.dart
+++ lib/widgets/news_list.dart
@@ -1,27 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:news_app/providers/news_provider.dart';
 import 'package:news_app/widgets/news_list_item.dart';
 
-class NewsList extends StatelessWidget {
+class NewsList extends ConsumerWidget {
   const NewsList({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return ListView(
-      itemExtent: 100,
+  Widget build(BuildContext context, ref) {
+    final newsState = ref.watch(newsProvider);
+
+    if (newsState.isLoading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return ListView.builder(
       padding: const EdgeInsets.all(16),
-      children: [
-        NewsListItem(),
-        NewsListItem(),
-        NewsListItem(),
-        NewsListItem(),
-        NewsListItem(),
-        NewsListItem(),
-        NewsListItem(),
-        NewsListItem(),
-        NewsListItem(),
-        NewsListItem(),
-        NewsListItem(),
-      ],
+      itemExtent: 100,
+      itemCount: newsState.news.length,
+      itemBuilder: (context, index) {
+        return NewsListItem(newsItem: newsState.news[index]);
+      },
     );
   }
 }
